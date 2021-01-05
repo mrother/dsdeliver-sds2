a46a8a9e43321411fef9eb0bf6c7c4144ed6bc42
Mauricio Rother <mauriciorother@gmail.com>
2021-01-05 16:44:14 -0500

    Initial Setup

--- backend/src/main/java/br/com/codered/com/br/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/br/com/codered/com/br/dsdeliver/DsdeliverApplication.java
@@ -1,4 +1,4 @@
-package br.com.codered.dsdeliver;
+package br.com.codered.com.br.dsdeliver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/br/com/codered/com/br/dsdeliver/DsdeliverApplicationTests.java
+++ backend/src/test/java/br/com/codered/com/br/dsdeliver/DsdeliverApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.codered.dsdeliver;
+package br.com.codered.com.br.dsdeliver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
